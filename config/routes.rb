3d5f055ed44b100797d76be01f622dc22f0ea59d
taiyou112 <tzty140@gmail.com>
2020-11-08 15:53:29 +0900

    自動更新の前まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update]
@@ -9,3 +10,9 @@ Rails.application.routes.draw do
     end
   end
 end
+=======
+  get 'messages/index'
+
+  root "messages#index"
+end
+>>>>>>> parent of 1aceb3b... git commit --allow-empty -m 'create pull request'
